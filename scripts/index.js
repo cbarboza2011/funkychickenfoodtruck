bc8ff6e34f7dc6c1483e4b7852602d464ef29328
cbarboza2011 <cbarboza2011@gmail.com>
2021-02-09 12:45:34 -0600

    fixing paypal

--- scripts/index.js
+++ scripts/index.js
@@ -16,26 +16,33 @@ $(document).ready(function() {
   }
 
   // Financing Steps
-  // $('.popup').hide();
-  // $('.finance-button').on('click', function() {
-  //   $('.truck').animate({marginRight: "200vw"}, 800);
-  //   $('.finance').animate({marginRight: "200vw"}, 800);
-  //   $('.popup').fadeIn(1000).animate({marginLeft: "0"}, 800);
-  //   $('.information').animate({marginLeft: "0"}, 800);
-  // });
-  // $('.form-group button').on('click', function() {
-  //   $('.information').animate({marginRight: "100vw"}, 800);
-  //
-  //   $('.information').animate({marginLeft: "0"}, 800);
-  // });
-  //
-  // // Loan Functions
-  // $('.slider').slider({
-  //   max: 38100,
-  //   min: 400,
-  //   step: 100,
-  //   value: 500,
-  // });
+  // $('.popup').hide();//hide the popup window on page load
+  $('.finance-button').on('click', function() { // after clickin donate or loan buttons, pull up information form to collect data
+    $buttonId = $(this).attr('id');
+    if ($buttonId == 'donatebutton') {
+      $('div.donation').addClass('selected')
+    } else if ($buttonId == 'loanbutton') {
+      $('div.loan').addClass('selected');
+    }
+    $('.truck').animate({marginRight: "200vw"}, 800);
+    $('.finance').animate({marginRight: "200vw"}, 800);
+    $('.popup').fadeIn(1000).animate({marginLeft: "0"}, 800);
+    $('.information').animate({marginLeft: "0"}, 800);
+  });
+
+  $('button.btn').on('click', function() { //form button submit
+    var $selected = $('.selected');
+    $('.information').animate({marginLeft: "-200vw"}, 800);
+    $($selected).animate({marginLeft: "0"}, 800);
+  });
+
+  // Loan Functions
+  $('.slider').slider({
+    max: 38100,
+    min: 400,
+    step: 100,
+    value: 500,
+  });
 
   function getLoanAmount() {
     var loanAmount = $('.loanamount');
@@ -52,38 +59,42 @@ $(document).ready(function() {
   $(".slider").on( "slidestop", function() {
     var $amount = $('.loanamount').text();
     $( ".slider" ).slider( "option", "value", $amount );
+    $('.paypalbutton').attr('amount', $amount);
   });
 
+  $('button.submitloan').on('click', function() {
+    $('.loan-setter').slideUp();
+    $('#smart-button-container').css('display', 'block');
+  });
 
-// AUTO GENERATED CODE FROM PAYPAL FOR BUTTON, MODIFIED
-  function initPayPalButton() {
-    paypal.Buttons({
-      style: {
-        shape: 'pill',
-        color: 'white',
-        layout: 'vertical',
-        label: 'paypal',
-
-      },
-
-      createOrder: function(data, actions) {
-        var $amount = $('.loanamount').text(); // I ADDED THIS TO GRAB LOAN AMOUNT FROM SLIDER
-        $('.dollaramount').text($amount);
-        return actions.order.create({
-          purchase_units: [{"amount":{"currency_code":"USD","value": $amount}}]// CHANGED BY ME from [{"amount":{"currency_code":"USD","value":1}}]
-        });
-      },
-
-      onApprove: function(data, actions) {
-        return actions.order.capture().then(function(details) {
-          alert('Transaction completed by ' + details.payer.name.given_name + '!');
-        });
-      },
-
-      onError: function(err) {
-        console.log(err);
-      }
-    }).render('#paypal-button-container');
-  }
-  initPayPalButton();
+  // function initPayPalButton() {
+  //   paypal.Buttons({
+  //     style: {
+  //       shape: 'pill',
+  //       color: 'white',
+  //       layout: 'vertical',
+  //       label: 'paypal',
+  //
+  //     },
+  //
+  //     createOrder: function(data, actions) {
+  //       var $amount = $('.loanamount').text();
+  //       $('.dollaramount').text($amount);
+  //       return actions.order.create({
+  //         purchase_units: [{"amount":{"currency_code":"USD","value": $amount}}]
+  //       });
+  //     },
+  //
+  //     onApprove: function(data, actions) {
+  //       return actions.order.capture().then(function(details) {
+  //         alert('Transaction completed by ' + details.payer.name.given_name + '!');
+  //       });
+  //     },
+  //
+  //     onError: function(err) {
+  //       console.log(err);
+  //     }
+  //   }).render('#paypal-button-container');
+  // }
+  // initPayPalButton();
 });
